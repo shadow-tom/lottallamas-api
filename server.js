47b53d492ccc64b13f98fa696064027185ae8c35
Tom Schnaars <tschnaars@shift4.com>
2022-12-06 23:46:39 -0500

    removal of IPFS/orbit

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ import express from 'express';
 import cors from 'cors';
 const app = express()
 
-import * as IPFS from 'ipfs-http-client'
-
-import OrbitDB from 'orbit-db'
-
 import auth from './routes/auth.js';
 import wallets from './routes/auth/wallets.js'
 import content from './routes/auth/content.js'
@@ -17,23 +13,6 @@ const port = 3100
 app.use(cors({
     origin: '*'
 }));
-
-if (process.env.NODE_ENV !== 'test') {
-	// Create IPFS/Orbit instance.  I placed this outside of the
-	// test suite as it was causing tests to hang
-	const ipfs = IPFS.create('http://localhost:5001');
-	const db = await OrbitDB.createInstance(ipfs)
-	
-	// TODO: Dry up this middleware
-	app.use(async (req, res, next) => {
-		req.db = db
-		req.db.wallets = await db.open('zdpuB1x7Ld2j5B8B6trBVCLrFb8cp527aZWQkCyZX6LsbyX5M/wallets', { indexBy: 'walletId' })
-		req.db.content = await db.open('zdpuAurkvYnqvTzoochJ2u8Vp8vbsHQwyxFbChneDzscNM1jk/content', { indexBy: 'walletId' })
-		await req.db.wallets.load();
-		await req.db.content.load();
-		next();
-	})
-}
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
